dde087c6b1e05de5f5c9f77b78b3c5ab44fd54d4
Sonu Pachauri <engg.cse12@gmail.com>
2023-01-18 00:14:17 +0530

    testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,15 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Product;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -16,6 +20,35 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
+        //will be pass
+        $response->assertSee('documentation');
+
+        // will be failed.
+       // $response->assertSee('symphony');
+
+
         $response->assertStatus(200);
+    }
+
+    public function test_homepage_contanins_product_list(){
+
+        $products= Product::create([
+            'title'=> 'Product 2',
+            'content'=> 'content',
+            'price'=> 10.10
+        ]);
+
+        //$products = Product::all();
+
+        $response = $this->get('/product');
+
+       
+        $response->assertStatus(200);
+        //$response->assertDontSee(__('test'));
+       $response->assertSee(__('Product 2'));
+
+        $response->assertViewHas('products',function($collection) use ($products){
+             return $collection->contains($products);
+        });
     }
 }
